162f3d8db74bf5fc7d0d13fe83857c46d88dfeb0
U201317876 <U201317876@upc.edu.pe>
2017-12-05 22:16:13 -0500

    Actualizacion del proyecto.
    Se modifico el script de la db
    Se agrega el proyecto Web

---
+++ WCF_Hoteles/WCF_Hoteles/Dominio/Destino.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WCF_Hoteles.Dominio
+{
+    public class Destino
+    {
+        [DataMember]
+        public int IdDistrito { get; set; }
+
+        [DataMember]
+        public int IdProvincia { get; set; }
+
+        [DataMember]
+        public int IdDepartamaneto { get; set; }
+
+        [DataMember]
+        public string NombreDestino { get; set; }
+    }
+}
